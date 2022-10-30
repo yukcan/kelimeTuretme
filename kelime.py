26b4aff1b8800581c2dc87d9ccd66bf244992d00
yukcan <78096072+yukcan@users.noreply.github.com>
2022-10-31 02:01:06 +0300

    Add files via upload

---
+++ kelime.py
@@ -0,0 +1,38 @@
+import random
+
+dosya = open("kelime.txt","r")
+kelime = dosya.read()
+kelimeler = []
+
+i = 0
+for x in range(len(kelime)):
+	if(ord(kelime[x])==10):
+		k = ""
+		for y in kelime[i:x]:
+			if(ord(y)>64 and ord(y)<91):
+				k += chr(ord(y)+32)
+			else:
+				k += y
+		kelimeler.append(k)
+		i = x+1
+#kelimeler dizisi
+print("sadece küçük harf")
+ilkKarakter = chr(random.randint(97,122))
+puan = 0
+yasakKelime = [""]
+while True:
+	print(ilkKarakter)
+	girilenKelime = input()
+	var = False
+	for x in kelimeler:
+		if(x==girilenKelime and girilenKelime[0]==ilkKarakter):
+			for y in yasakKelime:
+				if(girilenKelime!=y):
+					var = True
+					puan += 1
+					ilkKarakter = girilenKelime[len(girilenKelime)-1]
+					yasakKelime.append(girilenKelime)
+	print(var)
+	if(var == False):
+		print("puan=" +str(puan))
+		break
